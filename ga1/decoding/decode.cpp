2904eae9c6bd9994a4cf919dabb74f994f4ab9f0
Morgan Kortlander <morgankortlander@gmail.com>
2023-09-27 19:11:20 -0500

    u

--- ga1/decoding/decode.cpp
+++ ga1/decoding/decode.cpp
@@ -8,50 +8,70 @@
 #include <regex>
 #include <cstdio>
 
-using namespace std;
+
 
 /* //plan: find a ( then trigger another call if a ( is found before a ).
 
-overload function to accept iterator, so that can be passed in
+overload function to accept iterator, so that can be passed in IF there are parentheses
  */
+
+// Base function that accepts a string to decode. Should make a local copy of the string. Calls the overloaded version if it finds a '('
 std::string decode(std::string &toDec)
 {
+    char openChar = '(';
+    char closeChar = ')';
 
-    char opening = '(';
-    char closing = ')';
-    std::string::iterator itFirst = std::find_if(toDec.begin(), toDec.end(), [opening](char i){ return i == opening;});
-
-    if (itFirst != toDec.end())
+    // find opening (
+    std::string::iterator opening = std::find_if(toDec.begin(), toDec.end(), [openChar](char i)
+                                                 { return i == openChar; });
+    if (opening != toDec.end())
     {
-        std::string::iterator itFirst = std::find_if(toDec.begin(), toDec.end(), [opening](auto i){ return i == opening });
-        std::string::iterator itr2 = decode(toDec, itr);
-        std::string::iterator itrSecond = find ')';
+        decode("asdf", toDec.begin());
     }
-    for (i = itr1)
-
-    for (std::string::iterator itr = 
-    replace "(stuff)" with "ffuts" in situ 
     return toDec;
-} // notes: get iterator for the range, remove ( ) then use std::reverse then use std::string.replace to replace old substring with the fixed one
+}
+// notes: get iterator for the range, remove ( ) then use std::reverse then use std::string.replace to replace old substring with the fixed one
 
-std::string::iterator decode(std::string &part, std::string::iterator itr)
+// Overload that accepts a string and an iterator from its first instance of a '('. Since the string in this overloaded function is a reference, any changes made during recursive calls should carry over
+void decode(std::string &toDec, std::string::iterator previous)
 {
+    char openChar = '(';
+    char closeChar = ')';
 
-    /* std::string::iterator itr1 = std::find_if(toDec.begin) _, toDec.end() '(';
-    if (itr != end)
+    // find opening (, if any left
+    std::string::iterator opening = std::find_if(previous + 1, toDec.end(), [openChar](char i)
+                                                 { return i == openChar; });
+    if (opening != toDec.end())
     {
-    decode(&itr)
+        decode(toDec, opening);
     }
 
-    std::string::iterator itr2 = find ')';
-    for (std::string::iterator itr =
-    replace "(stuff)" with "ffuts" in situ  */
+    // find closing )
+    std::string::iterator closing = std::find_if(previous, toDec.end(), [closeChar](char i)
+                                                 { return i == closeChar; });
+
+    // pull out part to be reversed. Variable closing is technically a "beginning" iterator, so it doesn't need +1, since "ending iterators" are expected go one element past the last one !!!.
+    std::cout << "String before: " << toDec << std::endl;
+    std::string toReverse(opening + 1, closing);
+    std::cout << "Going to reverse: " << toReverse << std::endl;
+    std::reverse(toReverse.begin(), toReverse.end());
+    std::cout << "Reverse out: " << toReverse << std::endl;
+
+    // stick it back in, overwriting where there was ( ). !!! now we have the opposite situation with needing the +1 on closing, instead of opening.
+
+    toDec.replace(opening, closing + 1, toReverse);
+    std::cout << "String after replacement: " << toDec << std::endl;
 }
 
 int main()
 {
     freopen("myOutput.txt", "w", stdout);
-    std::ifstream inFile("input1.txt");
+
+    std::string testString = "(as)dbig(god)s"; //should be "sadbigdogs"
+    std::string decoded = decode(testString);
+    std::cout << decoded << std::endl;
+    return 0;
+    /* std::ifstream inFile("input1.txt");
     std::ofstream ofs("output1.txt");
 
     std::string line = "";
@@ -59,21 +79,21 @@ int main()
 
     while (getline(inFile >> std::ws, line))
     {
-    // >> std::ws eats white space before inserting to string, no need for removal lines
+        // >> std::ws eats white space before inserting to string, no need for removal lines
 
-    // receive bar
-    if (line == "Bar1" || line == "Bar2")
-    {
-        barName = line;
-        break; // restart loop with current bar
-    }
-    // edge case: no bar given yet, or emtpy
-    else if (line == "")
-        break;
+        // receive bar
+        if (line == "Bar1" || line == "Bar2")
+        {
+            barName = line;
+            break; // restart loop with current bar
+        }
+        // edge case: no bar given yet, or emtpy
+        else if (line == "")
+            break;
 
-    string encodedID = "";
-    string decodedID = "";
-    }
+        string encodedID = "";
+        string decodedID = "";
+    } */
 }
 
 /* void example(std::string perm)
