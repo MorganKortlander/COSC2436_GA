83aa01839cc21e32400dce96242624240adf51c7
Morgan Kortlander <morgankortlander@gmail.com>
2023-09-27 20:23:42 -0500

    decoding working on input file

--- ga1/decoding/decode.cpp
+++ ga1/decoding/decode.cpp
@@ -1,17 +1,14 @@
 #include <algorithm>
 #include <fstream>
 #include <iostream>
-#include <sstream>
 #include <string>
-#include <vector>
 #include <iostream>
-#include <regex>
 #include <cstdio>
 
-/* //plan: find a ( then trigger another call if a ( is found before a ).
-
-overload function to accept iterator, so that can be passed in IF there are parentheses
- */
+// check that 2 files opened correctly--function is defined below main
+bool filesOpened(std::ifstream &ifs, std::ofstream &ofs);
+// check that 3 files opened correctly--function is defined below main
+bool filesOpened(std::ifstream &ifs, std::ofstream &ofs, std::ifstream &cmd);
 
 // Overload that accepts a string and an iterator from its first instance of a '('. Since the string in this overloaded function is a reference, any changes made during recursive calls should carry over
 void decode(std::string &toDec, std::string::iterator previous)
@@ -20,160 +17,122 @@ void decode(std::string &toDec, std::string::iterator previous)
     char closeChar = ')';
 
     // find opening (, if any left
-    std::string::iterator opening = std::find_if(previous+1, toDec.end(), [openChar](char i)
+    std::string::iterator opening = std::find_if(previous + 1, toDec.end(), [openChar](char i)
                                                  { return i == openChar; });
     if (opening != toDec.end())
     {
         decode(toDec, opening);
     }
-
     // find closing )
-    std::string::iterator closing = std::find_if(previous+1, toDec.end(), [closeChar](char i)
+    std::string::iterator closing = std::find_if(previous + 1, toDec.end(), [closeChar](char i)
                                                  { return i == closeChar; });
-
-    // pull out part to be reversed. Variable closing is technically a "beginning" iterator, so it doesn't need +1, since "ending iterators" are expected go one element past the last one !!!.
     opening = previous;
-    std::cout << "String before: " << toDec << std::endl;
     std::string toReverse(opening + 1, closing);
-    std::cout << "Going to reverse: " << toReverse << std::endl;
     std::reverse(toReverse.begin(), toReverse.end());
-    std::cout << "Reverse out: " << toReverse << std::endl;
-
-    // stick it back in, overwriting where there was ( ). !!! now we have the opposite situation with needing the +1 on closing, instead of opening.
-
     toDec.replace(opening, closing + 1, toReverse);
-    std::cout << "String after replacement: " << toDec << std::endl;
+
 }
 
 // Base function that accepts a string to decode. Should make a local copy of the string. Calls the overloaded version if it finds a '('
-std::string decode(std::string &toDec)
+std::string decode(std::string toDec)
 {
     char openChar = '(';
     char closeChar = ')';
 
-    // find opening (
+    // find opening (, if any
     std::string::iterator opening = std::find_if(toDec.begin(), toDec.end(), [openChar](char i)
                                                  { return i == openChar; });
     if (opening != toDec.end())
     {
         decode(toDec, opening);
-        /* std::string::iterator closing = std::find_if(opening, toDec.end(), [closeChar](char i)
-                                                     { return i == closeChar; });
-
-        // pull out part to be reversed. Variable closing is technically a "beginning" iterator, so it doesn't need +1, since "ending iterators" are expected go one element past the last one !!!.
-
-        std::cout << "String before: " << toDec << std::endl;
-        std::string toReverse(opening + 1, closing);
-        std::cout << "Going to reverse: " << toReverse << std::endl;
-        std::reverse(toReverse.begin(), toReverse.end());
-        std::cout << "Reverse out: " << toReverse << std::endl;
-
-        // stick it back in, overwriting where there was ( ). !!! now we have the opposite situation with needing the +1 on closing, instead of opening.
-
-        toDec.replace(opening, closing + 1, toReverse);
-        std::cout << "String after replacement: " << toDec << std::endl; */
     }
     return toDec;
 }
-// notes: get iterator for the range, remove ( ) then use std::reverse then use std::string.replace to replace old substring with the fixed one
+// notes: gets iterator for the range, remove ( ) then use std::reverse then use std::string.replace to replace old substring with the fixed one
 
 int main()
 {
     freopen("myOutput.txt", "w", stdout);
 
-    std::string testString = "(as)dbig(god)s"; // should be "sadbigdogs"
+    /* std::string testString = "(as)dbig(god)s"; // should be "sadbigdogs"
     std::string decoded = decode(testString);
-    std::cout << decoded << std::endl;
-    return 0;
-    /* std::ifstream inFile("input1.txt");
+    std::cout << decoded << std::endl; */
+    std::ifstream inFile("input1.txt");
     std::ofstream ofs("output1.txt");
 
     std::string line = "";
-    std::string barName; // need to preserve barname between loops
+    std::string barName = ""; // need to preserve barname between loops
+    if (!filesOpened(inFile, ofs))
+    {
+        std::cerr << "Bad read" << std::endl;
+        return 1;
+    }
 
     while (getline(inFile >> std::ws, line))
     {
-        // >> std::ws eats white space before inserting to string, no need for removal lines
-
+        // >> std::ws eats white space before inserting to string. No need for removal lines!
+        std::cout << "Read in " << line << std::endl;
         // receive bar
+        std::string encodedID = "";
+        std::string decodedID = "";
         if (line == "Bar1" || line == "Bar2")
         {
             barName = line;
-            break; // restart loop with current bar
+            std::cout << "Detected " << line << std::endl;
+            continue; // restart loop with current bar
         }
         // edge case: no bar given yet, or emtpy
         else if (line == "")
-            break;
-
-        string encodedID = "";
-        string decodedID = "";
-    } */
+        {
+            std::cerr << "No bar!" << std::endl;
+            return 1;
+        }
+        else
+        {
+            decodedID = decode(line);
+            std::cout << "read in " << line << " and decoded as " << decodedID << std::endl;
+        }
+    }
 }
 
-/* void example(std::string perm)
-{ // example reverse iteration
-    std::string newPerm = "";
-    if (perm.size() < 2)
-    {
-    std::cout << "string size is < 2. Only one permutation" << std::endl;
-    return perm;
-    }
-    // base case 2: enough numbers but not already ordered
-    char swap = '0'; // domain is given as 0 to 10 not inclusive, so 0 won't appear in input file
-
-    // experiment with iterators in loop. str.end() points to address 1 over from last index, so use it+1 to compare location
-
-    // can reduce time by adding a passcount that reduces the checks by 1 each call following first, since each pass places the correct element at the end position
-
-    for (std::string::iterator it = perm.end() - 1; (it) != perm.begin(); --it)
-    {
-    std::cout << "Checking " << *it << " and " << *(it - 1) << std::endl;
-    if (*it > *(it - 1))
-    {
-        swap = *it;
-        *it = *(it - 1);
-        *(it - 1) = swap;
-    }
-    }
-} */
-
-/* std::string maxPerm(std::string perm, int count = 0) //
+bool filesOpened(std::ifstream &ifs, std::ofstream &ofs)
 {
+    bool opened = true;
+    if (!ofs.is_open())
+    {
+        std::cerr << "failed to open output file." << std::endl;
+        opened = false;
+        ofs.close();
+    }
+    if (!ifs.is_open())
+    {
+        std::cerr << "failed to open input file." << std::endl;
+        opened = false;
+        ifs.close();
+    }
+    return opened;
+}
 
-    // sentry value
-    if (count > 1000)
+bool filesOpened(std::ifstream &ifs, std::ofstream &ofs, std::ifstream &cmd)
+{
+    bool opened = true;
+    if (!ofs.is_open())
     {
-    std::cout << "Exceeded 1000 iterations. Exiting with error" << std::endl;
-    exit(EXIT_FAILURE);
+        std::cerr << "failed to open output file." << std::endl;
+        opened = false;
+        ofs.close();
     }
-    // base case 1: 1 or fewer numbers; no change needed
-    std::string newPerm = "";
-    if (perm.size() < 2)
+    if (!ifs.is_open())
     {
-    std::cout << "string size is < 2. Only one permutation" << std::endl;
-    return perm;
+        std::cerr << "failed to open input file." << std::endl;
+        opened = false;
+        ifs.close();
     }
-    // base case 2: enough numbers but not already ordered
-    char swap = '0'; // domain is given as 0 to 10 not inclusive, so 0 won't appear in input file
-
-    // experiment with iterators in loop. str.end() points to address 1 over from last index, so use it+1 to compare location
-
-    // can reduce time by adding a passcount that reduces the checks by 1 each call following first, since each pass places the correct element at the end position
-    for (std::string::iterator it = perm.begin(); (it + 1) != perm.end(); ++it)
+    if (!cmd.is_open())
     {
-    std::cout << "Checking " << *it << " and " << *(it + 1) << std::endl;
-    if (*it < *(it + 1))
-    {
-        swap = *it;
-        *it = *(it + 1);
-        *(it + 1) = swap;
+        std::cerr << "failed to open command file." << std::endl;
+        opened = false;
+        cmd.close();
     }
-    }
-    std::cout << "Current perm: " << perm << std::endl;
-    // case 3: no change happened, swap still = '0'
-    if (swap == '0')
-    return perm;
-    count++;
-    // case 4: change happened, check again
-    return maxPerm(perm, count);
-} */
+    return opened;
+}
